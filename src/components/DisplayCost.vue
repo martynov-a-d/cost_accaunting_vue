908eeccfa217c02c7d7a420f0fe28f639d12b2b1
Alex Martynov <a.martynov@216base.ru>
2022-10-07 12:47:28 +0300

    Create skeleton for project

---
+++ src/components/DisplayCost.vue
@@ -0,0 +1,54 @@
+<template>
+
+    <div>
+        <h1>COST</h1>
+        <!-- Data output component -->
+        <div class="cost_elements">
+            <div class="cost_element" v-for="elem in this.costList" v-bind:key="elem.id">
+                <p>{{ elem.name }}</p>
+                <p>{{ elem.price }}</p>
+            </div>
+        </div>
+        <input type="text" v-model="this.newCost.price">
+        <button v-on:click="costAdder">ADD COST</button>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: "DisplayCostVue",
+    data() {
+        return {
+            costList: [
+                {
+                    id: 1,
+                    name: "eat",
+                    price: 250,
+                },
+                {
+                    id: 2,
+                    name: "home",
+                    price: 15000,
+                },
+            ],
+            newCost: {
+                id: '',
+                name: '',
+                price: 0,
+            }
+        }
+    },
+    methods: {
+        costAdder() {
+            console.log('')
+        },
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+@import "../assets/DisplayCost.scss";
+</style>
