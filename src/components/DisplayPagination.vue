348cdbdaa5ee7e51722daa69771488916f36552c
Alex Martynov <a.martynov@216base.ru>
2022-10-18 10:41:29 +0300

    Add logic to pagination

--- src/components/DisplayPagination.vue
+++ src/components/DisplayPagination.vue
@@ -2,9 +2,9 @@
 
     <div>
         <!-- Data output component -->
-        <DataOutput v-bind:costList="this.costList" />
-        <button v-for="elem in costPagination" v-bind:key="elem.id">{{ elem.name }}</button>
-        <p>"Длинна ={{ number }}"</p>
+        <DataOutput v-bind:costList="this.pageList" />
+        <button v-for="elem in costPagination" v-bind:key="elem.id" v-on:click="choisePagination(elem.id)">{{ elem.name
+        }}</button>
     </div>
 
 </template>
@@ -16,27 +16,41 @@ export default {
     props: {
         costList: {
             type: Array,
+            default: function () { [] },
         }
     },
     data() {
         return {
-            number: this.costList.length,
             costPagination: [],
             pagesPagination: 5,
+            pageList: [],
+            padList: [],
+            pageSelect: "",
         }
     },
-    created() {
+    beforeMount() {
+    },
+    mounted() {
         this.pageHandler()
+        this.choisePagination(1)
     },
     beforeUpdate() {
-        this.pageHandler()
-        this.paginationHandler()
+        this.paginationClean()
+        this.pageListUpdater()
+        this.updatePaginstion()
     },
     methods: {
+        pageListUpdater() {
+            this.padList = this.costList
+        },
+        paginationClean() {
+            this.costPagination = []
+            this.pageHandler()
+        },
         pageHandler() {
             let remainderDivision = this.costList.length / this.pagesPagination;
             if (this.costList.length % this.pagesPagination == 0) {
-                for (let i = 1; i < remainderDivision; i++) {
+                for (let i = 1; i <= remainderDivision; i++) {
                     this.costPagination.push({ id: i, name: i })
                 }
             } else {
@@ -45,9 +59,18 @@ export default {
                 }
             }
         },
-        paginationHandler() {
-            this.number = this.costList.length
+        choisePagination(elem) {
+            this.pageSelect = elem
+            this.updatePaginstion()
+        },
+        updatePaginstion() {
+            let page = this.pageSelect * this.pagesPagination
+            this.pageList = this.padList
+            this.pageList = this.pageList.slice([page - 5], [page])
         }
+    },
+    computed: {
+
     }
 }
 
