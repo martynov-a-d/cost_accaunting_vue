3dfa73d37f83f1736b08749b496f62fc347df492
Alex Martynov <a.martynov@216base.ru>
2023-05-25 10:27:43 +0300

    Refactoring router component

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import DisplayCostVue from "../components/DisplayCost"
-import About from "../components/About"
-import Page404 from "../components/Page404"
-import DisplayAdder from "../components/DisplayAdder"
 Vue.use(Router);
 
 const router = new Router({
@@ -16,27 +12,27 @@ const router = new Router({
         {
             path: "/dashboard/:page",
             name: "dashboard",
-            component: DisplayCostVue
+            component: () => import("../components/DisplayCost")
         },
         {
             path: "/add/payment",
             name: "addPayment",
-            component: DisplayAdder
+            component: () => import("../components/DisplayAdder")
         },
         {
             path: "/add/payment/:category",
             name: "addPaymentURL",
-            component: DisplayAdder
+            component: () => import("../components/DisplayAdder")
         },
         {
             path: "/about",
             name: "about",
-            component: About
+            component: () => import("../components/About")
         },
         {
             path: "/404",
             name: "notFound",
-            component: Page404
+            component: () => import("../components/Page404")
         },
         {
             path: "*",
